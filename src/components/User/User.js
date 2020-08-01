673c157a8a9db8731b7c52817d80058d93100709
Susan Holland <segh@fastmail.com>
2020-08-01 18:01:49 -0400

    Added shadow to profile images

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -5,7 +5,7 @@ export default function User(props) {
     return (
         <tr>
             <td>
-                <img src={props.image} className="circle left" />
+                <img src={props.image} className="circle left z-depth-2" />
                 <h4 className="flow-text center-align">{props.name}</h4>
             </td>
             <td>{props.role}</td>
