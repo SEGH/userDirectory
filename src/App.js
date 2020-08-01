e09a7cbab84cd0617ef114768629503be249dfde
Susan Holland <segh@fastmail.com>
2020-08-01 13:55:08 -0400

    Added Header component with css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "./components/Header/Header";
 import Table from "./components/Table/Table";
 
 export default function App() {
     return (
         <>
-            <h1>Hello!</h1>
+            <Header />
             <Table />
         </>
     )
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./header.css";
+
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <div className="nav-wrapper valign-wrapper indigo darken-4">
+                        <a href="#" className="brand-logo center">This Old House</a>
+                </div>
+            </nav>
+        </header>
+    )
+}
