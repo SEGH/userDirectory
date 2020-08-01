5f9e0c150216fff59c9e2e4730d1734b49de2202
Susan Holland <segh@fastmail.com>
2020-08-01 11:43:14 -0400

    Added Table component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Table from "./components/Table/Table";
 
 export default function App() {
     return (
         <>
             <h1>Hello!</h1>
+            <Table />
         </>
     )
 }
---
+++ src/components/Table/Table.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Table() {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Role</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+        </table>
+    )
+}
